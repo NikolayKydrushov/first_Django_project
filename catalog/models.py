d9f9c4e755c94d19b8c4ba92f5474e27f231aced
Nikolay Kydrushov <nnikolay2909@gmail.com>
2026-02-04 22:35:19 +0300

    working with Django shell second

--- catalog/models.py
+++ catalog/models.py
@@ -14,8 +14,6 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-    product4 = Product.objects.create(name='Дуб', description='Вайб', picture='', category=category2, purchase_price=9000)
-
     # 1. Product:
     #     * наименование,
     #     * описание,
